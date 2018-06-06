59e9854eece51dfb0687ff2641c5de8d2d08822a
Serj <sergii.dzugas@gmail.com>
2018-06-06 22:58:19 +0300

    add form

---
+++ form/main.js
@@ -0,0 +1,20 @@
+"use strict"
+
+function myForm() {
+    var firstName = document.getElementById("firstName").value;
+    var lastName = document.getElementById("lastName").value;
+    var emailIndex = document.getElementById("email").value.indexOf("@");
+    if (firstName === "") {
+        alert("Name must be filled out");
+        return false;
+    };
+    if (lastName === "") {
+        alert("Name must be filled out");
+        return false;
+    };
+    if (emailIndex === -1) {
+        alert("Email must be valid");
+        return false;
+    };
+    alert("Submitted");
+}
